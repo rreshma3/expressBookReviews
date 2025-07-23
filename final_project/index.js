1f869805c1edd163f70d66e78c17ea64c53d0e22
rreshma3 <reshmaramesh032002@gmail.com>
2025-07-23 21:15:18 +0530

    Add files via upload

---
+++ final_project/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const session = require('express-session');
+// const { authenticated } = require('./router/auth_users.js');
+const customer_routes = require('./router/auth_users.js').authenticated;
+const genl_routes = require('./router/general.js').general;
+const books_route = require('./router/booksdb.js');
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+
+app.use("/books", books_route);
+
+
+app.use("/customer/auth/*", function auth(req, res, next) {
+    // Check if the user is authenticated via session
+
+        // Check if there's a JWT token in the request headers
+console.log(req.session)
+
+        if(req.session){
+            if(req.session.authorization){
+                    let token = req.session.authorization.token;
+                    jwt.verify(token, 'my-secret-key', (err, user) => {
+                        if (err) {
+                            // Token verification failed
+                            res.status(401).json({ error: 'galek err' });
+                        } else {
+                            req.session.user = user;
+                            next();
+                        }
+                    });
+
+
+            }
+            else{
+                res.status(401).json({ error: 'Unauthorized here' });
+            }
+        }
+
+
+    
+});
+
+ 
+const PORT =3000;
+
+app.use("/customer", customer_routes);
+app.use("/", genl_routes);
+
+app.listen(PORT,()=>console.log("Server is running"));
+
---
+++ final_project/router/general.js
@@ -0,0 +1,214 @@
+const express = require('express');
+let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+const axios = require("axios");
+const public_users = express.Router();
+
+
+public_users.use(express.json());
+
+
+
+
+public_users.post("/register", (req,res) => {
+  //Write your code here
+  let username = req.body.username;
+  let password = req.body.password;
+  console.log(req.body)
+  if(username && password){
+    if(isValid(username)){
+      users.push({username:username,password:password});
+      return res.status(200).json({message: "User registered successfully"});
+    }
+    else{
+      return res.status(400).json({message: "Username already exists"});
+    }
+  }else{
+    return res.status(400).json({message: "Username or password not provided"});
+  }
+
+
+});
+//task 1 
+// Get the book list available in the shop
+// public_users.get('/', async function (req, res) {
+
+//   if (books) {
+//     return res.status(200).send(JSON.stringify(books, null, 4));
+//   } else {
+//     return res.status(404).json({ message: "No books found" });
+//   }
+// });
+
+
+public_users.get('/', async function (req, res) {
+  //Write your code here
+  console.log('hello m here');
+  axios.get('http://localhost:5000/books').then(
+    (responseBooks)=>{
+      return res.status(200).send(JSON.stringify(responseBooks.data,null , 4));
+    }
+  ).catch(e=>
+    res.status(404).send("cant get books <br>  "+ e)
+    )
+});
+
+
+
+// Get book details based on ISBN
+
+//task 2
+// public_users.get('/isbn/:isbn',function (req, res) {
+//   //Write your code here
+//   let isbn = req.params.isbn;
+//   if(books[isbn]){
+//     return res.status(200).send(JSON.stringify(books[isbn],null,4));
+//   }
+//   else{
+//     return res.status(404).send("No book found with ISBN "+isbn);
+//   }
+
+//  });
+  
+ // task 11
+public_users.get('/isbn/:isbn', async function (req, res) {
+
+  let isbn = req.params.isbn;
+
+  try {
+    const response = await axios.get('http://localhost:5000/books');
+
+    if (response.data[isbn]) {
+      return res.status(200).send(JSON.stringify(response.data[isbn], null, 4));
+    } else {
+      return res.status(404).send("No book found with ISBN " + isbn);
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+
+
+
+
+
+
+
+// Get book details based on author 
+//task 3
+// public_users.get('/author/:author',function (req, res) {
+//   //Write your code here
+//   let author = req.params.author;
+//   let booksByAuthor = [];
+//   for(let isbn in books){
+//     if(books[isbn].author == author){
+//       booksByAuthor.push(books[isbn]);
+//     }
+//   }
+//   if(booksByAuthor.length>0){
+//     return res.status(200).send(JSON.stringify(booksByAuthor,null,4));
+//   }
+//   else{
+//     return res.status(404).send("No book found with author "+author);
+//   }
+// });
+
+//task 12
+public_users.get('/author/:author', async function (req, res) {
+
+  let author = req.params.author;
+  let booksByAuthor = [];
+
+  try {
+
+    const response = await axios.get('http://localhost:5000/books');
+
+    for (let isbn in response.data) {
+      if (response.data[isbn].author == author) {
+        booksByAuthor.push(response.data[isbn]);
+      }
+    }
+
+    if (booksByAuthor.length > 0) {
+      return res.status(200).send(JSON.stringify(booksByAuthor, null, 4));
+    } else {
+      return res.status(404).send("No book found with author " + author);
+    }
+  } catch (error) {
+
+    console.error(error);
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//task 13
+public_users.get('/title/:title', async function (req, res) {
+
+  let title = req.params.title;
+  let booksByTitle = [];
+
+  try {
+    const response = await axios.get('http://localhost:5000/books');
+
+    for (let isbn in response.data) {
+      if (response.data[isbn].title == title) {
+        booksByTitle.push(response.data[isbn]);
+      }
+    }
+
+    if (booksByTitle.length > 0) {
+      return res.status(200).send(JSON.stringify(booksByTitle, null, 4));
+    } else {
+      return res.status(404).send("No book found with title " + title);
+    }
+  } catch (error) {
+
+    console.error(error);
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  Get book review
+public_users.get('/review/:isbn',function (req, res) {
+  //Write your code here
+  let isbn = req.params.isbn;
+  if(books[isbn]){
+    return res.status(200).send(JSON.stringify(books[isbn].reviews,null,4));
+  }
+  else{
+    return res.status(404).send("No book found with ISBN "+isbn);
+  }
+});
+
+module.exports.general = public_users;
